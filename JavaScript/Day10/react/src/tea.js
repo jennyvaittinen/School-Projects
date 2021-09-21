6843403d08bbc6a7c22e34e0b5e338a5394b2fc3
jennyvaittinen <73814101+jennyvaittinen@users.noreply.github.com>
2021-09-21 18:19:42 +0300

    added projects

---
+++ JavaScript/Day10/react/src/tea.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var otsikko = React.createElement("div", null, React.createElement("h1", null, "Teen p\xE4\xE4luokat"), React.createElement("p", null, "Teen voidaan jakaa esim. kuuteen p\xE4\xE4luokkaan teeuutoksen v\xE4rin perusteella. T\xE4m\xE4k\xE4\xE4n ei ole kuitenkaan t\xE4ysin toimiva luokittelu."), React.createElement("ul", null, React.createElement("li", null, "valkoinen tee"), React.createElement("li", null, "keltainen tee"), React.createElement("li", null, "vihre\xE4 tee"), React.createElement("li", null, "oolong tee"), React.createElement("li", null, "musta tee"), React.createElement("li", null, "tumma tee")), React.createElement("img", { src: "https://cdn.pixabay.com/photo/2015/07/02/20/37/cup-829527_1280.jpg" }));
+
+var mesta = document.getElementById("paikka");
+
+ReactDOM.render(otsikko, mesta);
